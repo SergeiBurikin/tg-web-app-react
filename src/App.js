cc4710db7214d5a15783920a177de01ad3ddf37c
SergeiBurikin <almighty2806@gmail.com>
2024-02-21 16:33:26 +0700

    init

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
 
 
     return (
-    <div className="App">
-        <Header />
-        <button onClick={onToggleButton}>toggle</button>
-    </div>
+        <div className="App">
+            <Header />
+            <button onClick={onToggleButton}>toggle</button>
+        </div>
     );
 }
 
